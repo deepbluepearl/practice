24178ca8778251dc12278a92a2957801e0004c11
ted <tedlet>
2016-12-24 22:51:36 -0600

    modified:   samples.py

--- samples.py
+++ samples.py
@@ -7,21 +7,24 @@ print("string exercises")
 #easy way: go through the alphabet and just count each
 #other way:  get letter 1 -> make sure its not in list -> count -> add to list -> 2nd letter, repeat
 
-def letter_count(arg_string):
-    string_matrix = []
-    print(string_matrix[0])
-    print("/n")
-    string_matrix.append([])
+def letter_count(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+    # not my answer, copied from w3
 
-    for i in range(0,len(arg_string)):
-        if arg_string[i] not in string_matrix[i]:
-            string_matrix.append([])
-            string_matrix[i] = []
-            string_matrix[i].append(arg_string[i])
-            string_matrix[i].append(arg_string.count(arg_string[i]))
-            #string_matrix.append(0)
+# print(letter_count('ted'))
 
-    return string_matrix
+# get a string made of the first 2 and the last 2 chars from a given a string.
+# If the string length is less than 2, return instead of the empty string
+def string_2s(str_arg):
+    if len(str_arg) > 1:
+        return str_arg[0:2] + str_arg[-2:]
+    return ""
 
-
-print(letter_count("hello"))
+print(string_2s("ab"))
